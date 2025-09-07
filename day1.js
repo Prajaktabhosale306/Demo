686d19be6f4700cf334013c226530148798ae5b5
Prajaktabhosale306 <Prajaktabhosale306@gmail.com>
2025-09-07 21:14:44 +0530

    Day 1: Setup & Basics - added JS script

---
+++ day1.js
@@ -0,0 +1,11 @@
+// day1.js
+
+// store your details
+const name = "Prajakta"; // replace with your actual name
+const age = 25;          // replace with your actual age
+const learning = "Cypress";
+
+// print them
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Currently Learning:", learning);
